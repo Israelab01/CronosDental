a1a5b2a87287b571703ee02bc0299be61af2c498
Israelab01 <israelab001@gmail.com>
2025-06-16 18:41:42 +0200

    Backend finised

--- backend/app/Http/Controllers/OrderController.php
+++ backend/app/Http/Controllers/OrderController.php
@@ -8,8 +8,18 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     // Return all orders
-    public function index()
-    {
-        return Order::all();
+    public function index(Request $request)
+{
+    $status = $request->query('status');
+    $perPage = 5;
+
+    $query = Order::query();
+    
+    if ($status) {
+        $query->where('status', $status);
     }
+
+    return $query->paginate($perPage);
+}
+
 }
